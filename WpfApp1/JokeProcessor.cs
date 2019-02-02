162266e1c67f11184e3bac80cfc036d3c569f45f
Edward <Edward@DESKTOP-V62I65P>
2019-02-02 10:54:00 -0500

    Init Commit

---
+++ WpfApp1/JokeProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class JokeProcessor
+    {
+        public static async Task<JokeModel> LoadJoke()
+        {
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync("http://api.icndb.com/jokes/random"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    JokeModelValue result = await response.Content.ReadAsAsync<JokeModelValue>();
+                    return result.Value;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static HttpClient client = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ApiHelper.InitilizeClient();
+        }
+
+        private async Task LoadJoke()
+        {
+            var joke = await JokeProcessor.LoadJoke();
+            infoTextBox.Text = joke.Joke;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await LoadJoke();
+        }
+    }
+}
+
